3deb0d114468dd20d5c735a56f7ac9f8c3dae278
aditek-marcosfavarao <marcos.favarao@aditek.com.br>
2023-10-23 11:55:39 -0300

    feature: add datalist input in edition module

---
+++ src/app/data/treatment.ts
@@ -0,0 +1,40 @@
+import { Treatment } from '../core/@types/treatment';
+
+export const treatments: Treatment[] = [
+  {
+    id: 'complete',
+    type: 'Complete',
+  },
+  {
+    id: 'pedido-refinamento',
+    type: 'Pedido de refinamento',
+  },
+  {
+    id: 'retainer',
+    type: 'Retainer',
+  },
+  {
+    id: 'reposição',
+    type: 'Alinhador de reposição',
+  },
+  {
+    id: 'colagem-indireta',
+    type: 'Colagem indireta',
+  },
+  {
+    id: 'you-plan',
+    type: 'You plan',
+  },
+  {
+    id: 'self-plan',
+    type: 'Self plan',
+  },
+  {
+    id: 'ajuste-fase',
+    type: 'Ajuste de fase',
+  },
+  {
+    id: 'print-3d',
+    type: 'Print 3D',
+  },
+];
--- src/app/modules/editor/editor.component.ts
+++ src/app/modules/editor/editor.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Treatment } from 'src/app/core/@types/treatment';
+import { treatments } from 'src/app/data/treatment';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css'],
 })
-export class EditorComponent {}
+export class EditorComponent {
+  treatments: Treatment[] = treatments;
+}
