86728e625d7ff37f45d744c49633ba15133624fb
Ana Berigo <ana.berigo@aditek.com.br>
2023-10-25 08:56:15 -0300

    Criando interface profile.

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -7,39 +7,6 @@ import { DataServiceService } from 'src/app/core/services/data-service.service';
 
 type ProfileClass = 'pcenter' | 'pspace';
 
-
-export interface Profile {
-  id: string;
-  appointment: {
-    lastApointment: Date;
-    nextApointment: Date;
-  };
-  treatment: {
-    treatmentType: string;
-    treatmentStart: Date;
-    treatmentEnd: Date;
-  };
-  user: {
-    name: string;
-    id: string;
-    document: string;
-    birthdate: Date;
-    age: number;
-    phone: string;
-    celphone: string;
-    email: string;
-    address: {
-      address: string;
-      number: number;
-      complement: string;
-      city: string;
-      uf: string;
-      cep: string;
-    };
-  };
-  medicineObservations: string;
-  observations: string;
-}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
